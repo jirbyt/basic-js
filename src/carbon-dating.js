7d4681dcfd1bd36ea0e7e46fd09cc8730d1b6fe6
jirbyt <jirbyt@yandex.ru>
2020-10-05 18:50:27 +0300

    fix: fix carbon-dating, hanoi-tower

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+if(typeof sampleActivity === 'string' && parseFloat(sampleActivity)>=1 && parseFloat(sampleActivity)<15) {
+let k = 0.693/HALF_LIFE_PERIOD;
+let t = Math.ceil(Math.abs(Math.log(sampleActivity/MODERN_ACTIVITY)/k));
+return t;
+}
+else{
+return false;
+}
 };
