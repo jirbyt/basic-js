4c764a54eccaea4e2777e5e028dcc40049f7742f
jirbyt <jirbyt@yandex.ru>
2020-10-07 18:50:05 +0300

    fix: extended-repeater and vigenere-cipher done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+let result =str+'',
+resultEnd =''
+separatorNew = '',
+additionSeparatorNew='',
+additionNew=''; 
+
+if (typeof str !==undefined){
+
+options['separator']===undefined ? separatorNew = '+' : separatorNew= options['separator'];
+options['additionSeparator']===undefined ? additionSeparatorNew = '|' : additionSeparatorNew= options['additionSeparator'];
+options['addition']===undefined ? additionNew = '' : additionNew= options['addition'];
+if (additionNew !=='' ){
+let i = options['additionRepeatTimes'];
+do{result+=additionNew;
+i>1 ?   result+=additionSeparatorNew: result+='';
+i--;
+}
+while(i>0);
+}
+
+
+if (options['repeatTimes']>1){
+let i = options['repeatTimes'];
+do{resultEnd+=result;
+i>1 ?   resultEnd+=separatorNew: resultEnd+='';
+i--;
+}
+while(i>0);
+}
+else {resultEnd=result;}
+
+
+
+}
+
+else  throw new Error();
+
+return resultEnd
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,84 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  typeOfMachine;
+
+constructor(typeOfMachine){
+this.typeOfMachine = typeOfMachine;
+}
+
+encrypt(message, key) {
+ 
+
+let arrMess = message.toUpperCase().split('').map(item => item.charCodeAt() - 65);
+let arrKey =  key.toUpperCase().split('').map(item => item.charCodeAt() - 65);
+
+let indexOfSpace = [];
+for(let i=0; i<arrMess.length; i++){
+arrMess[i]>=0 && arrMess[i]<26 ? indexOfSpace[i]='' : indexOfSpace[i]=i;  
+}
+indexOfSpace = indexOfSpace.filter(item => item !== '');
+
+arrMess = arrMess.filter(item => item>=0&&item<26);
+
+for(let i=0; i<arrMess.length; i++){
+arrKey = arrKey.concat(arrKey);
+if (arrKey.length>arrMess.length)  break; 
+}
+arrKey=arrKey.splice(0, arrMess.length)
+
+
+let result = [];
+for(let i=0; i<arrMess.length; i++){
+result[i]=arrMess[i]+arrKey[i]
+}
+
+result = result.map(item => item<26 ? String.fromCharCode(item+65):String.fromCharCode(item+39))
+
+for(let i=0; i<indexOfSpace.length; i++){
+result.splice(indexOfSpace[i], 0, message.split('')[indexOfSpace[i]] )
+}
+
+
+this.typeOfMachine ===false ? result = result.reverse().join('') :  result = result.join('');
+return result
+}
+
+decrypt(message, key) {
+ 
+
+let arrMess = message.toUpperCase().split('').map(item => item.charCodeAt() - 65);
+let arrKey =  key.toUpperCase().split('').map(item => item.charCodeAt() - 65);
+
+let indexOfSpace = [];
+for(let i=0; i<arrMess.length; i++){
+arrMess[i]>=0 && arrMess[i]<26 ? indexOfSpace[i]='' : indexOfSpace[i]=i;
+}
+
+indexOfSpace = indexOfSpace.filter(item => item !== '');
+
+arrMess = arrMess.filter(item => item>=0&&item<26);
+
+for(let i=0; i<arrMess.length; i++){
+arrKey = arrKey.concat(arrKey);
+if (arrKey.length>arrMess.length)  break; 
+}
+arrKey=arrKey.splice(0, arrMess.length)
+
+let result = [];
+for(let i=0; i<arrMess.length; i++){
+result[i]=arrMess[i]-arrKey[i]
+}
+
+result = result.map(item => item>=0 ? String.fromCharCode(item+65):String.fromCharCode(item+91))
+
+for(let i=0; i<indexOfSpace.length; i++){
+result.splice(indexOfSpace[i], 0, message.split('')[indexOfSpace[i]] )
+}
+
+this.typeOfMachine ===false ? result = result.reverse().join('') :  result = result.join('');
+return result
+}
 }
 
 module.exports = VigenereCipheringMachine;
